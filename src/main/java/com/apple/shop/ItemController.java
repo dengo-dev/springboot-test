0e63a0bdc55532d605308b619f65c5a2823247f0
dengo <dengo23.dev@gmail.com>
2024-12-01 15:05:42 +0900

    상세페이지 만들기

--- src/main/java/com/apple/shop/ItemController.java
+++ src/main/java/com/apple/shop/ItemController.java
@@ -1,15 +1,52 @@
 package com.apple.shop;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+  private final ItemRepository itemRepository;
 
   @GetMapping("/list")
   public String list(Model model) {
-    model.addAttribute("name", "비싼바지");
+    List<Item> result = itemRepository.findAll();
+    model.addAttribute("items", result);
+
     return "list.html";
+  }
+
+  @GetMapping("/write")
+  public String write() {
+    return "write.html";
+  }
+
+  @PostMapping("/add")
+  public String addPost(@RequestParam String title,
+                        @RequestParam Integer price) {
+    Item item = new Item();
+    item.setTitle(title);
+    item.setPrice(price);
+    itemRepository.save(item);
+    return "redirect:/list";
+  }
+
+  @GetMapping("/detail/{id}")
+  public String detail(@PathVariable Long id, Model model) {
+    Optional<Item> result = itemRepository.findById(id);
+    if (result.isPresent()) {
+      model.addAttribute("data", result.get());
+      return "detail.html";
+
+    } else {
+      return "rediret:/list";
+    }
   }
 }
